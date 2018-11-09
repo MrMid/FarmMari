86aa87ade1aaa46a1bfa9706942822e6c26abae6
elkanio <lukaskania2@gmail.com>
2018-11-09 22:39:56 +0100

    dsa
    das

--- HackatonSeznam/www.seznam.cz/temp/cache/latte/presenters-templates-@layout.latte--77999178c5.php
+++ HackatonSeznam/www.seznam.cz/temp/cache/latte/presenters-templates-@layout.latte--77999178c5.php
@@ -100,25 +100,6 @@ class Template77999178c5 extends Latte\Runtime\Template
 
         <!-- Header -->
         <header>
-            <!-- Top Navbar -->
-            <div class="top_nav">
-                <div class="container">
-                    <ul class="list-inline info">
-                        <li><a href="#"><span class="fa fa-phone"></span> 1234 - 5678 - 9012</a></li>
-                        <li><a href="#"><span class="fa fa-envelope"></span> support@Construct.com</a></li>
-                        <li><a href="#"><span class="fa fa-clock-o"></span> Mon - Sat 9:00 - 19:00</a></li>
-                    </ul>
-                    <ul class="list-inline social_icon">
-                        <li><a href=""><span class="fa fa-facebook"></span></a></li>
-                        <li><a href=""><span class="fa fa-twitter"></span></a></li>
-                        <li><a href=""><span class="fa fa-behance"></span></a></li>
-                        <li><a href=""><span class="fa fa-dribbble"></span></a></li>
-                        <li><a href=""><span class="fa fa-linkedin"></span></a></li>
-                        <li><a href=""><span class="fa fa-youtube"></span></a></li>
-                    </ul>			
-                </div>
-            </div><!-- Top Navbar end -->
-
             <!-- Navbar -->
             <nav class="navbar bootsnav">
                 <!-- Top Search -->
@@ -183,135 +164,13 @@ class Template77999178c5 extends Latte\Runtime\Template
             </div>
         </section><!-- Services end -->
 
-        <!-- Testimonial -->
-        <section id="testimonial">
-            <div class="container text-center testimonial_area">
-                <h2>Customer Reviews</h2>
-                <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,</p>
-
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="testimonial_item">
-                            <div class="testimonial_content text-left">
-                                <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,</p>
-                            </div>
-                            <img src="images/testimonial_img1.png" alt="Testimonial">
-                            <p class="worker_name">john smith</p>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="testimonial_item">
-                            <div class="testimonial_content">
-                                <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,</p>
-                            </div>
-                            <img src="images/testimonial_img2.png" alt="Testimonial">
-                            <p class="worker_name">john smith</p>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="testimonial_item">
-                            <div class="testimonial_content">
-                                <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,</p>
-                            </div>
-                            <img src="images/testimonial_img1.png" alt="Testimonial">
-                            <p class="worker_name">john smith</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section><!-- Testimonial end -->
-
-        <!-- Contact form -->
-        <section id="contact_form">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h2>Do you have any questions?</h2>
-                        <h2 class="second_heading">Feel free to contact us!</h2>
-                    </div>
-                    <form role="form" class="form-inline text-right col-md-6" >
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="name" placeholder="Name">
-                        </div>
-                        <div class="form-group">
-                            <input type="email" class="form-control" id="email" placeholder="Email">
-                        </div>
-                        <div class="form-group">
-                            <textarea class="form-control" rows="5" id="msg" placeholder="Message"></textarea>
-                        </div>
-                        <button type="submit" class="btn submit_btn">Submit</button>
-                    </form>				
-                </div>
-            </div>
-        </section><!-- Contact form end -->
-
+        
         <!-- Footer -->
         <footer>
-            <!-- Footer top -->
-            <div class="container footer_top">
-                <div class="row">
-                    <div class="col-lg-4 col-sm-7">
-                        <div class="footer_item">
-                            <h4>About Company</h4>
-                            <img class="logo" src="images/logo.png" alt="Construction">
-                            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem</p>
-
-                            <ul class="list-inline footer_social_icon">
-                                <li><a href=""><span class="fa fa-facebook"></span></a></li>
-                                <li><a href=""><span class="fa fa-twitter"></span></a></li>
-                                <li><a href=""><span class="fa fa-youtube"></span></a></li>
-                                <li><a href=""><span class="fa fa-google-plus"></span></a></li>
-                                <li><a href=""><span class="fa fa-linkedin"></span></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-sm-5">
-                        <div class="footer_item">
-                            <h4>Explore link</h4>
-                            <ul class="list-unstyled footer_menu">
-                                <li><a href=""><span class="fa fa-play"></span> Our services</a>
-                                <li><a href=""><span class="fa fa-play"></span> Meet our team</a>
-                                <li><a href=""><span class="fa fa-play"></span> Forum</a>
-                                <li><a href=""><span class="fa fa-play"></span> Help center</a>
-                                <li><a href=""><span class="fa fa-play"></span> Contact Cekas</a>
-                                <li><a href=""><span class="fa fa-play"></span> Privacy Policy</a>
-                                <li><a href=""><span class="fa fa-play"></span> Cekas terms</a>
-                                <li><a href=""><span class="fa fa-play"></span> Site map</a>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-7">
-                        <div class="footer_item">
-                            <h4>Latest post</h4>
-                            <ul class="list-unstyled post">
-                                <li><a href=""><span class="date">20 <small>AUG</small></span>  Luptatum omittantur duo ne mpetus indoctum</a></li>
-                                <li><a href=""><span class="date">20 <small>AUG</small></span>  Luptatum omittantur duo ne mpetus indoctum</a></li>
-                                <li><a href=""><span class="date">20 <small>AUG</small></span>  Luptatum omittantur duo ne mpetus indoctum</a></li>
-                                <li><a href=""><span class="date">20 <small>AUG</small></span>  Luptatum omittantur duo ne mpetus indoctum</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-5">
-                        <div class="footer_item">
-                            <h4>Contact us</h4>
-                            <ul class="list-unstyled footer_contact">
-                                <li><a href=""><span class="fa fa-map-marker"></span> 124 New Line, London UK</a></li>
-                                <li><a href=""><span class="fa fa-envelope"></span> hello@psdfreebies.com</a></li>
-                                <li><a href=""><span class="fa fa-mobile"></span><p>+44 00 00 1234 <br>+44 00 00 1234</p></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div><!-- Footer top end -->
-
             <!-- Footer bottom -->
             <div class="footer_bottom text-center">
                 <p class="wow fadeInRight">
-                    Made with 
-                    <i class="fa fa-heart"></i>
-                    by 
-                    <a target="_blank" href="http://bootstrapthemes.co">Bootstrap Themes</a> 
-                    2016. All Rights Reserved
+                    Tým Farmáři.
                 </p>
             </div><!-- Footer bottom end -->
         </footer><!-- Footer end -->
@@ -351,14 +210,14 @@ class Template77999178c5 extends Latte\Runtime\Template
 	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 	<script src="https://nette.github.io/resources/js/netteForms.min.js"></script>
 	
-	<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 287 */ ?>/js/jquery-1.12.1.min.js"></script>
-    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 288 */ ?>/js/bootstrap.min.js"></script>
-    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 289 */ ?>/js/bootsnav.js"></script>
-    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 290 */ ?>/js/isotope.js"></script>
-    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 291 */ ?>/js/isotope-active.js"></script>
-    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 292 */ ?>/js/jquery.fancybox.js?v=2.1.5"></script>
-    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 293 */ ?>/js/jquery.scrollUp.min.js"></script>
-    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 294 */ ?>/js/main.js"></script>
+	<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 146 */ ?>/js/jquery-1.12.1.min.js"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 147 */ ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 148 */ ?>/js/bootsnav.js"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 149 */ ?>/js/isotope.js"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 150 */ ?>/js/isotope-active.js"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 151 */ ?>/js/jquery.fancybox.js?v=2.1.5"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 152 */ ?>/js/jquery.scrollUp.min.js"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 153 */ ?>/js/main.js"></script>
     
 <?php
 	}
