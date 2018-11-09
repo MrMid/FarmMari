105f5c790f6a6d05001eb7963ed774aacecec0bd
Jan Tichý <jan.tichy@tul.cz>
2018-11-09 23:17:18 +0100

    query add

---
+++ HackatonSeznam/www.seznam.cz/temp/cache/latte/presenters-templates-@layout.latte--3d542762fe.php
@@ -0,0 +1,173 @@
+<?php
+// source: C:\xampp\htdocs\hackaton\FarmMari\HackatonSeznam\www.seznam.cz\app\presenters/templates/@layout.latte
+
+use Latte\Runtime as LR;
+
+class Template3d542762fe extends Latte\Runtime\Template
+{
+	public $blocks = [
+		'head' => 'blockHead',
+		'scripts' => 'blockScripts',
+	];
+
+	public $blockTypes = [
+		'head' => 'html',
+		'scripts' => 'html',
+	];
+
+
+	function main()
+	{
+		extract($this->params);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+	<title><?php
+		if (isset($this->blockQueue["title"])) {
+			$this->renderBlock('title', $this->params, function ($s, $type) {
+				$_fi = new LR\FilterInfo($type);
+				return LR\Filters::convertTo($_fi, 'html', $this->filters->filterContent('striphtml', $_fi, $s));
+			});
+			?> | <?php
+		}
+?>Fake News</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- <link rel="stylesheet" href="<?php echo LR\Filters::escapeHtmlComment($basePath) /* line 13 */ ?>/css/style.css"> -->
+	<?php
+		if ($this->getParentName()) return get_defined_vars();
+		$this->renderBlock('head', get_defined_vars());
+?>
+	
+	
+		<!-- Custom Fonts -->
+        <link rel="stylesheet" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 18 */ ?>/custom-font/fonts.css">
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 20 */ ?>/css/bootstrap.min.css">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 22 */ ?>/css/font-awesome.min.css">
+        <!-- Bootsnav -->
+        <link rel="stylesheet" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 24 */ ?>/css/bootsnav.css">
+        <!-- Fancybox -->
+        <link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 26 */ ?>/css/jquery.fancybox.css?v=2.1.5" media="screen">	
+        <!-- Custom stylesheet -->
+        <link rel="stylesheet" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 28 */ ?>/css/custom.css">
+        <!--[if lt IE 9]>
+                <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+</head>
+
+<body>
+<?php
+		$iterations = 0;
+		foreach ($flashes as $flash) {
+			?>	<div<?php if ($_tmp = array_filter(['flash', $flash->type])) echo ' class="', LR\Filters::escapeHtmlAttr(implode(" ", array_unique($_tmp))), '"' ?>><?php
+			echo LR\Filters::escapeHtmlText($flash->message) /* line 35 */ ?></div>
+<?php
+			$iterations++;
+		}
+?>
+	
+	<!-- Preloader -->
+
+        <div id="loading">
+            <div id="loading-center">
+                <div id="loading-center-absolute">
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                </div>
+            </div>
+        </div>
+
+        <!--End off Preloader -->
+
+        <!-- Header -->
+        <header>
+            <!-- Navbar -->
+            <nav class="navbar bootsnav">
+                <!-- Top Search -->
+                <div class="top-search">
+                    <div class="container">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                    </div>
+                </div>  
+            </nav><!-- Navbar end -->
+        </header><!-- Header end -->
+
+<?php
+		$this->renderBlock('content', $this->params, 'html');
+?>
+
+        
+        <!-- Footer -->
+        <footer>
+            <!-- Footer bottom -->
+            <div class="footer_bottom text-center">
+                <p class="wow fadeInRight">
+                    Tým Farmáři.
+                </p>
+            </div><!-- Footer bottom end -->
+        </footer><!-- Footer end -->
+	
+	
+	
+
+<?php
+		$this->renderBlock('scripts', get_defined_vars());
+?>
+</body>
+</html>
+<?php
+		return get_defined_vars();
+	}
+
+
+	function prepare()
+	{
+		extract($this->params);
+		if (isset($this->params['flash'])) trigger_error('Variable $flash overwritten in foreach on line 35');
+		Nette\Bridges\ApplicationLatte\UIRuntime::initialize($this, $this->parentName, $this->blocks);
+		
+	}
+
+
+	function blockHead($_args)
+	{
+		
+	}
+
+
+	function blockScripts($_args)
+	{
+		extract($_args);
+?>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	<script src="https://nette.github.io/resources/js/netteForms.min.js"></script>
+	
+	<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 94 */ ?>/js/jquery-1.12.1.min.js"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 95 */ ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 96 */ ?>/js/bootsnav.js"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 97 */ ?>/js/isotope.js"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 98 */ ?>/js/isotope-active.js"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 99 */ ?>/js/jquery.fancybox.js?v=2.1.5"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 100 */ ?>/js/jquery.scrollUp.min.js"></script>
+    <script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 101 */ ?>/js/main.js"></script>
+    
+<?php
+	}
+
+}
---
+++ HackatonSeznam/www.seznam.cz/temp/cache/latte/templates-Homepage-default.latte--247b1c1ceb.php
@@ -0,0 +1,70 @@
+<?php
+// source: C:\xampp\htdocs\hackaton\FarmMari\HackatonSeznam\www.seznam.cz\app\presenters/templates/Homepage/default.latte
+
+use Latte\Runtime as LR;
+
+class Template247b1c1ceb extends Latte\Runtime\Template
+{
+	public $blocks = [
+		'content' => 'blockContent',
+	];
+
+	public $blockTypes = [
+		'content' => 'html',
+	];
+
+
+	function main()
+	{
+		extract($this->params);
+		if ($this->getParentName()) return get_defined_vars();
+		$this->renderBlock('content', get_defined_vars());
+		return get_defined_vars();
+	}
+
+
+	function prepare()
+	{
+		extract($this->params);
+		Nette\Bridges\ApplicationLatte\UIRuntime::initialize($this, $this->parentName, $this->blocks);
+		
+	}
+
+
+	function blockContent($_args)
+	{
+?>        <section id="services">
+            <div class="container">
+                <h2>OUR SERVICES</h2>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="service_item">
+                            <img src="images/service_img1.jpg" alt="Our Services">
+                            <h3>CONSTRUCTION MANAGEMENT</h3>
+                            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,</p>
+                            <a href="#services" class="btn know_btn">know more</a>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="service_item">
+                            <img src="images/service_img2.jpg" alt="Our Services">
+                            <h3>RENOVATION</h3>
+                            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,</p>
+                            <a href="#services" class="btn know_btn">know more</a>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="service_item">
+                            <img src="images/service_img3.jpg" alt="Our Services">
+                            <h3>ARCHITECTURE</h3>
+                            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,</p>
+                            <a href="#services" class="btn know_btn">know more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+<?php
+	}
+
+}
