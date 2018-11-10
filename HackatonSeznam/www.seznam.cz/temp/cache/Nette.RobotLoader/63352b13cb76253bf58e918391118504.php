8546e6bb40049414395e96d79f2724ae3db465d0
elkanio <lukaskania2@gmail.com>
2018-11-10 14:51:37 +0100

    update

--- HackatonSeznam/www.seznam.cz/temp/cache/Nette.RobotLoader/63352b13cb76253bf58e918391118504.php
+++ HackatonSeznam/www.seznam.cz/temp/cache/Nette.RobotLoader/63352b13cb76253bf58e918391118504.php
@@ -55,7 +55,7 @@ return array (
     'App\\Presenters\\HomepagePresenter' => 
     array (
       'file' => 'C:\\wamp64\\www\\FarmMari\\HackatonSeznam\\www.seznam.cz\\app\\presenters\\HomepagePresenter.php',
-      'time' => 1541843839,
+      'time' => 1541853695,
     ),
   ),
   1 => 
--- HackatonSeznam/www.seznam.cz/temp/cache/latte/templates-Homepage-default.latte--5af3f7e4d6.php
+++ HackatonSeznam/www.seznam.cz/temp/cache/latte/templates-Homepage-default.latte--5af3f7e4d6.php
@@ -44,13 +44,16 @@ class Template5af3f7e4d6 extends Latte\Runtime\Template
                 <div class="top-search">
                     <div class="container">
                         <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                             <span class="input-group-addon"><i class="fa fa-search"></i></span> 
                             <?php
 		/* line 12 */
 		echo Nette\Bridges\FormsLatte\Runtime::renderFormBegin($form = $_form = $this->global->formsStack[] = $this->global->uiControl["search"], ['role'=>"form"]);
 ?>
 
                                 <?php echo end($this->global->formsStack)["query"]->getControl()->addAttributes(['class'=>"form-control", 'placeholder'=>"Search"]) /* line 13 */ ?>
+
+            					<?php echo end($this->global->formsStack)["search"]->getControl()->addAttributes(['class'=>'tlacitkoSearch']) /* line 14 */ ?>
+
 
                             <?php
 		echo Nette\Bridges\FormsLatte\Runtime::renderFormEnd(array_pop($this->global->formsStack));
@@ -93,7 +96,34 @@ class Template5af3f7e4d6 extends Latte\Runtime\Template
 				<a href="#"><p class="url">https://www.novinky.cz/domaci/485359-zeman-poblahopral-drahosovi-je-to-cena-utechy.html</p></a>	
 		        <p class="description">Přízviska Miloše Zemana se během let různila: muž, který pozvedl ČSSD ze dna, pragmatický premiér, vulgární premiér, unavený premiér, neúspěšný kandidát na prezidenta, zatrpklý důchodce z Vysočiny.</p>
 		    </div>
-		    
+
+           <div class="zaznam">
+                <a href="#"><p class="title">Zeman poblahopřál Drahošovi: Je to to cena útěchy …</p></a>
+                <a href="#"><p class="url">https://www.novinky.cz/domaci/485359-zeman-poblahopral-drahosovi-je-to-cena-utechy.html</p></a>  
+                <p class="description">Přízviska Miloše Zemana se během let různila: muž, který pozvedl ČSSD ze dna, pragmatický premiér, vulgární premiér, unavený premiér, neúspěšný kandidát na prezidenta, zatrpklý důchodce z Vysočiny.</p>
+            </div>
+                       <div class="zaznam">
+                <a href="#"><p class="title">Zeman poblahopřál Drahošovi: Je to to cena útěchy …</p></a>
+                <a href="#"><p class="url">https://www.novinky.cz/domaci/485359-zeman-poblahopral-drahosovi-je-to-cena-utechy.html</p></a>  
+                <p class="description">Přízviska Miloše Zemana se během let různila: muž, který pozvedl ČSSD ze dna, pragmatický premiér, vulgární premiér, unavený premiér, neúspěšný kandidát na prezidenta, zatrpklý důchodce z Vysočiny.</p>
+            </div>
+                       <div class="zaznam">
+                <a href="#"><p class="title">Zeman poblahopřál Drahošovi: Je to to cena útěchy …</p></a>
+                <a href="#"><p class="url">https://www.novinky.cz/domaci/485359-zeman-poblahopral-drahosovi-je-to-cena-utechy.html</p></a>  
+                <p class="description">Přízviska Miloše Zemana se během let různila: muž, který pozvedl ČSSD ze dna, pragmatický premiér, vulgární premiér, unavený premiér, neúspěšný kandidát na prezidenta, zatrpklý důchodce z Vysočiny.</p>
+            </div>
+                       <div class="zaznam">
+                <a href="#"><p class="title">Zeman poblahopřál Drahošovi: Je to to cena útěchy …</p></a>
+                <a href="#"><p class="url">https://www.novinky.cz/domaci/485359-zeman-poblahopral-drahosovi-je-to-cena-utechy.html</p></a>  
+                <p class="description">Přízviska Miloše Zemana se během let různila: muž, který pozvedl ČSSD ze dna, pragmatický premiér, vulgární premiér, unavený premiér, neúspěšný kandidát na prezidenta, zatrpklý důchodce z Vysočiny.</p>
+            </div>
+
+           <div class="zaznam">
+                <a href="#"><p class="title">Zeman poblahopřál Drahošovi: Je to to cena útěchy …</p></a>
+                <a href="#"><p class="url">https://www.novinky.cz/domaci/485359-zeman-poblahopral-drahosovi-je-to-cena-utechy.html</p></a>  
+                <p class="description">Přízviska Miloše Zemana se během let různila: muž, který pozvedl ČSSD ze dna, pragmatický premiér, vulgární premiér, unavený premiér, neúspěšný kandidát na prezidenta, zatrpklý důchodce z Vysočiny.</p>
+            </div>
+                        		    
 		    <div class="zaznam">
 		        <a href="#"><p class="title">Zeman odkládá cesty. Prezident oproti plánům nepojede do …</p></a>
 				<a href="#"><p class="url">https://www.kupi.cz/letaky/zeman-maso-uzeniny</p></a>	
